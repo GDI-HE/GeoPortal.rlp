8f833fc82a0457dd61a76b55103c53044394dc19
dpakprajul <bindaaspratiks@gmail.com>
2024-04-16 12:25:15 +0200

    Fix undefined variable error in frontpage.js

--- useroperations/static/useroperations/js/frontpage.js
+++ useroperations/static/useroperations/js/frontpage.js
@@ -643,6 +643,7 @@ function loadPage(pageNum, checkNextPage = false, sort_by = 'rank') {
             var start = (currentSet - 1) *5 + 1;
             var end = Math.min(currentSet * 5, totalPages);
             // Populate the pagination with buttons
+            var GoToPage, Back, Next, GoToPrevious, GoToNext, SeeAll, ShowAllWMCs; //define variable so that it won't create console error when the variable is not defined
             for (var i = 1; i <= 5; i++) {
                 var pageNumber = (currentSet - 1) * 5 + i;
                 var link = $('<a>', {
