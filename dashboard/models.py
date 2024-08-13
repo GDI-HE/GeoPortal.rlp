3b895e7c977ba9b2bc085452c3f18fdeea0dda4d
dpakprajul <bindaaspratiks@gmail.com>
2024-08-13 14:31:11 +0200

    Refactor SessionData model and add timestamp_create field

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class SessionData(models.Model):
+	sn = models.AutoField(primary_key=True)
+	timestamp_create = models.DateTimeField()
+	number_of_user = models.IntegerField()
+
+	def __str__(self):
+		return f"Session {self.sn} - {self.timestamp_create} - Users: {self.number_of_user}"
+
+	class Meta:
+		db_table = "session_data"
+		
