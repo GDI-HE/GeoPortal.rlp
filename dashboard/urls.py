dd05d205affa0dbaa6c0c6db57533cc28f9db439
dpakprajul <bindaaspratiks@gmail.com>
2025-02-12 09:09:03 +0100

    #https://github.com/GDI-HE/dashboard/issues/14 removed unnecessary function

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -11,8 +11,6 @@ urlpatterns = [
     #path('dashboard/upload_file', views.upload_file, name='upload_file'),
     path('filter/', views.filter, name='filter'),
     path('download_csv/', views.download_csv, name='download_csv'),
-     #delete some which are not necessary in future
-    path('service-quality/<int:resource_id>/', views.display_service_quality, name='display_service_quality'),
     path('dashboard/', include(extra_patterns)),
     path('layer-keywords/<int:layer_id>/', views.get_layer_keywords, name='layer_keywords'),
     path('load-more-data/', views.load_more_data, name='load_more_data'),
