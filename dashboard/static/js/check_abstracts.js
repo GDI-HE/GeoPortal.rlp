97c10d1cd1818c70d406b8073cc2e9fef85af597
dpakprajul <bindaaspratiks@gmail.com>
2025-03-18 09:19:50 +0100

    https://github.com/GDI-HE/dashboard/issues/3 clean the code and moved css, js to their respective file.

--- dashboard/static/js/check_abstracts.js
+++ dashboard/static/js/check_abstracts.js
@@ -6,235 +6,6 @@ function toggleDropdown(button) {
     button.textContent = dropdownContent.classList.contains('show') ? 'Hide Details' : 'Show Details';
 }
 
-        function searchTable() {
-    const input = document.getElementById('searchInput');
-    const filter = input.value.toLowerCase();
-    if (filter.length == 0) {
-        location.reload();
-        return;
-    } 
-    if (filter.length < 5) {
-        return; // Do not search until at least 5 characters are entered
-    }
-   
-    const spinner = document.getElementById('spinner');
-    spinner.style.display = 'block'; // Show the spinner
-    const loadMoreButton = document.getElementById('load-more');
-    loadMoreButton.style.display = 'none'; // Hide the load more button
-    fetch(`/search-data?query=${filter}`)
-        .then(response => response.json())
-        .then(data => {
-            const tableBody = document.querySelector('#serviceTable tbody');
-            tableBody.innerHTML = ''; // Clear existing rows
-            let serialNo = 0;
-            data.results.forEach(result => {
-                serialNo += 1; // Increment serial number for each new row
-                const mainRow = document.createElement('tr');
-                mainRow.classList.add('main-row');
-                const titleClass = result.layers_with_comma_keywords.length;
-                const layersWithoutAbstractHtml = result.layers_without_abstract
-                ? '<i class="fas fa-circle red-dot"></i>'
-                : '<i class="fas fa-circle green-dot"></i>';
-
-                const layersWithoutKeywordsHtml = result.layers_without_keywords
-                ? '<i class="fas fa-circle red-dot"></i>'
-                : '<i class="fas fa-circle green-dot"></i>';
-
-                let layersAbstractMatchHtml = '';
-                if (Array.isArray(result.layers_abstract_match)) {
-                    layersAbstractMatchHtml = result.layers_abstract_match.map(match => {
-                        if (match === 'Y') {
-                            return '<i class="fas fa-circle red-dot"></i>';
-                        } else if (match === 'N') {
-                            return '<i class="fas fa-circle green-dot"></i>';
-                        }
-                        return '';
-                    }).join('');
-                } else if (typeof result.layers_abstract_match === 'string') {
-                    layersAbstractMatchHtml = result.layers_abstract_match.split('').map(match => {
-                        if (match === 'Y') {
-                            return '<i class="fas fa-circle red-dot"></i>';
-                        } else if (match === 'N') {
-                            return '<i class="fas fa-circle green-dot"></i>';
-                        }
-                        return '';
-                    }).join('');
-                } else {
-                    console.warn('layers_abstract_match is not an array or string:', result.layers_abstract_match);
-                }
-
-                const layersWithShortAbstractCountHtml = result.layers_with_short_abstract_count > 0
-                ? '<i class="fas fa-circle red-dot"></i>'
-                : '<i class="fas fa-circle green-dot"></i>';
-
-                const connectedWmsHtml = result.connected_wms
-                    ? '<i class="fas fa-circle green-dot"></i>'
-                    : '<i class="fas fa-circle red-dot"></i>';
-
-                mainRow.innerHTML = `
-                    <td>${serialNo}</td>
-                    <td>${result.wms_id}</td>
-                    <td class="${titleClass}">${result.wms_title}</td>
-                    <td>${result.total_layers}</td>
-                    <td>${layersWithoutAbstractHtml}</td>
-                    <td>${layersWithoutKeywordsHtml}</td>
-                    <td>${layersAbstractMatchHtml}</td>
-                    <td>${layersWithShortAbstractCountHtml}</td>
-                    <td>${connectedWmsHtml}</td>
-                   
-                    <td>
-                        <button class="dropdown-btn" onclick="toggleDropdown(this)">Show Details</button>
-                    </td>
-                `;
-
-                const detailRow = document.createElement('tr');
-                detailRow.classList.add('details-section');
-                detailRow.innerHTML = `
-                  <td colspan="12">
-                    <div class="dropdown-container">
-                        <div class="dropdown">
-                            <div class="dropdown-title">
-                                Keywords Present
-                                ${result.keywords_present.length > 0 
-                                    ? '<i class="fas fa-info-circle"></i>' 
-                                    : '<i class="fas fa-info-circle red-icon"></i>'}
-                            </div>
-                            <div class="dropdown-content">
-                                <ul>
-                                    ${result.keywords_present.map(keyword => `<li>${keyword}</li>`).join('')}
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="dropdown">
-                            <div class="dropdown-title">
-                            Abstracts Present
-                            ${result.abstracts_present.length > 0 
-                                ? '<i class="fas fa-info-circle"></i>' 
-                                : '<i class="fas fa-info-circle red-icon"></i>'}
-                            </div>
-                            <div class="dropdown-content">
-                                <ul>
-                                    ${result.abstracts_present.map(abstract => `<li>${abstract}</li>`).join('')}
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="dropdown">
-                            <div class="dropdown-title">
-                                Layers Without Abstracts
-                                ${result.layers_without_abstract_names.length > 0 
-                                    ? '<i class="fas fa-info-circle red-icon"></i>' 
-                                    : '<i class="fas fa-info-circle"></i>'}
-                            </div>
-                            <div class="dropdown-content">
-                                <ul>
-                                    ${result.layers_without_abstract_names.map(layer_name => `<li>${layer_name}</li>`).join('')}
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="dropdown">
-                            <div class="dropdown-title">
-                            Layers Without Keywords
-                            ${result.layers_without_keyword_names.length > 0 
-                                ? '<i class="fas fa-info-circle red-icon"></i>' 
-                                : '<i class="fas fa-info-circle"></i>'}
-                            </div>
-                            <div class="dropdown-content">
-                                <ul>
-                                    ${result.layers_without_keyword_names.map(layer_name => `<li>${layer_name}</li>`).join('')}
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="dropdown">
-                            <div class="dropdown-title">
-                            Layers Where Abstract Matches Title
-                            ${result.layers_abstract_matches_title_names.length > 0 
-                                ? '<i class="fas fa-info-circle red-icon"></i>' 
-                                : '<i class="fas fa-info-circle"></i>'}
-                            </div>
-                            <div class="dropdown-content">
-                                <ul>
-                                    ${result.layers_abstract_matches_title_names.map(layer_name => `<li>${layer_name}</li>`).join('')}
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="dropdown">
-                            <div class="dropdown-title">
-                            Layer Names
-                            ${result.layer_names.length > 0 
-                                ? '<i class="fas fa-info-circle"></i>' 
-                                : '<i class="fas fa-info-circle red-icon"></i>'}
-                            </div>
-                            <div class="dropdown-content">
-                                <ul>
-                                    ${result.layer_names.map(layer_name => `<li>${layer_name}</li>`).join('')}
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="dropdown">
-                            <div class="dropdown-title">
-                            Layers With Short Abstracts
-                            ${result.layers_with_short_abstract_info.length > 0 
-                                ? '<i class="fas fa-info-circle red-icon"></i>' 
-                                : '<i class="fas fa-info-circle"></i>'}
-                            </div>
-                            <div class="dropdown-content">
-                                <ul>
-                                    ${result.layers_with_short_abstract_info.map(info => `<li class="short-abstract">${info[0]}: ${info[1]}</li>`).join('')}
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="dropdown">
-                            <div class="dropdown-title">
-                            Angaben zu Kosten/Gebühren/Lizenzen
-                             ${result.wms_fees && result.wms_fees.length > 5
-                                ? '<i class="fas fa-info-circle"></i>' 
-                                : '<i class="fas fa-info-circle red-icon"></i>'}
-                            </div>
-                            <div class="dropdown-content">
-                                <ul>
-                                    ${result.wms_fees}
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="dropdown">
-                            <div class="dropdown-title">
-                            Beschränkungen des öffentlichen Zugangs
-                                ${result.wms_accessconstraints && result.wms_accessconstraints.length > 5
-                                    ? '<i class="fas fa-info-circle"></i>' 
-                                    : '<i class="fas fa-info-circle red-icon"></i>'}
-                            </div>
-                            <div class="dropdown-content">
-                                <ul>
-                                    ${result.wms_accessconstraints}
-                                </ul>
-                            </div>
-                        </div>
-                       </div> 
-                    </td>
-                `;
-
-                tableBody.appendChild(mainRow);
-                tableBody.appendChild(detailRow);
-            });
-            var dropdowns = document.querySelectorAll('.dropdown');
-            dropdowns.forEach(function(dropdown){
-                var title = dropdown.querySelector('.dropdown-title');
-                var content = dropdown.querySelector('.dropdown-content');
-                content.style.display = 'none';
-                title.addEventListener('click', function(){
-                    var isVisible = content.style.display === 'block';
-                    content.style.display = isVisible ? 'none' : 'block';
-                    title.classList.toggle('active', !isVisible);
-                } )
-            } )
-        })
-        .finally(() => {
-            spinner.style.display = 'none'; // Hide the spinner
-        }
-        );
-}
 function toggleClearButton(){
     const input = document.getElementById('searchInput');
     const clearButton = document.getElementById('clear-input1');
@@ -392,3 +163,120 @@ document.addEventListener('DOMContentLoaded', function() {
             });
     });
 });
+
+function toggleColumnList() {
+    const columnList = document.getElementById('columnToggleList');
+    if (columnList.style.display === 'none') {
+        columnList.style.display = 'block';
+    } else {
+        // Check the number of selected columns
+        const selectedColumns = document.querySelectorAll('#columnToggleList input[type="checkbox"]:checked');
+        columnList.style.display = 'none';
+    } 
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const tooltipTrigger = document.getElementById("tooltip-trigger");
+    const tooltipContent = document.getElementById("tooltip-content");
+  
+    tooltipTrigger.addEventListener("click", function (event) {
+      event.stopPropagation();
+      tooltipContent.classList.toggle("show-tooltip");
+  
+      if (tooltipContent.classList.contains("show-tooltip")) {
+        tooltipContent.focus();
+      }
+    });
+  
+    tooltipTrigger.addEventListener("keydown", function (event) {
+      if (event.key === "Enter" || event.key === " ") {
+        event.stopPropagation();
+        tooltipContent.classList.toggle("show-tooltip");
+  
+        if (tooltipContent.classList.contains("show-tooltip")) {
+          tooltipContent.focus();
+        }
+      }
+    });
+  
+    document.addEventListener("click", function () {
+      if (tooltipContent.classList.contains("show-tooltip")) {
+        tooltipContent.classList.remove("show-tooltip");
+      }
+    });
+  });
+  
+  // Add this function at the beginning of your script
+function resetColumnToggles() {
+    document.querySelectorAll('#columnToggleList input[type="checkbox"]').forEach(checkbox => {
+        const column = parseInt(checkbox.getAttribute('data-column'));
+        const table = document.getElementById('serviceTable');
+        const th = table.querySelector(`thead th:nth-child(${column + 1})`);
+        const tds = table.querySelectorAll(`tbody td:nth-child(${column + 1})`);
+
+        // Check if the checkbox was initially checked in the HTML
+        const initiallyChecked = checkbox.hasAttribute('checked');
+
+        if (initiallyChecked) {
+            // Keep initially checked columns visible and checked
+            checkbox.checked = true;
+            th.style.display = '';
+            tds.forEach(td => td.style.display = '');
+        } else {
+            // Reset only the manually checked checkboxes
+            checkbox.checked = false;
+            th.style.display = 'none';
+            tds.forEach(td => td.style.display = 'none');
+        }
+    });
+}
+
+// Call this function when the page loads
+document.addEventListener('DOMContentLoaded', resetColumnToggles);
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('#columnToggleList input[type="checkbox"]').forEach(checkbox => {
+        checkbox.addEventListener('change', function () {
+            const column = this.getAttribute('data-column');
+            const table = document.getElementById('serviceTable');
+            const th = table.querySelector(`thead th:nth-child(${parseInt(column) + 1})`);
+            const tds = table.querySelectorAll(`tbody td:nth-child(${parseInt(column) + 1})`);
+            if (this.checked) {
+                th.style.display = '';
+                tds.forEach(td => td.style.display = '');
+            } else {
+                th.style.display = 'none';
+                tds.forEach(td => td.style.display = 'none');
+            }
+        });
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+document.querySelectorAll('.dropdown-btn').forEach(button => {
+    button.addEventListener('click', () => {
+      const detailsSection = button.closest('tr').nextElementSibling;
+      if (detailsSection.classList.contains('details-section')) {
+        // Toggle between showing and hiding the details section
+        detailsSection.style.display = 
+          detailsSection.style.display === 'none' || detailsSection.style.display === '' 
+          ? 'table-row' 
+          : 'none';
+      }
+    });
+  });
+}); 
+
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdowns = document.querySelectorAll('.dropdown');
+    dropdowns.forEach(function(dropdown) {
+        var title = dropdown.querySelector('.dropdown-title');
+        var content = dropdown.querySelector('.dropdown-content');
+        content.style.display = 'none';
+        title.addEventListener('click', function() {
+            var isVisible = content.style.display === 'block';
+            content.style.display = isVisible ? 'none' : 'block';
+            title.classList.toggle('active', !isVisible);
+        });
+    });
+});
+
